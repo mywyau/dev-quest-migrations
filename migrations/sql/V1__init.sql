eb97741e92b265601c11b9e8ff0c3d085edcf8eb
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-05-29 13:00:27 +0100

    wip

--- migrations/sql/V1__init.sql
+++ migrations/sql/V1__init.sql
@@ -1,3 +1,17 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS quests;
+DROP TABLE IF EXISTS reward;
+
+CREATE TABLE users (
+    user_id VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    user_type VARCHAR(50) CHECK (user_type IN ('Client', 'Dev', 'UnknownUserType')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Quests table
 CREATE TABLE quests (
     id BIGSERIAL PRIMARY KEY,
@@ -10,4 +24,13 @@ CREATE TABLE quests (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
+-- Reward table
+CREATE TABLE reward (
+    id BIGSERIAL PRIMARY KEY,
+    quest_id BIGINT NOT NULL REFERENCES quests(id),
+    base_reward NUMERIC NOT NULL,
+    time_reward NUMERIC NOT NULL,
+    completion_reward NUMERIC NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
