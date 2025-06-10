d4c53838d29bbf3f5bc93f07a3d4c2638e197e80
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-06-10 19:46:30 +0100

    adding delete cascades based on users

--- migrations/sql/V1__init.sql
+++ migrations/sql/V1__init.sql
@@ -1,7 +1,9 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS quests;
+DROP TABLE IF EXISTS dev_submissions;
 DROP TABLE IF EXISTS reward;
+DROP TABLE IF EXISTS quests;
+DROP TABLE IF EXISTS users;
 
+-- Users table
 CREATE TABLE users (
     user_id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
@@ -15,23 +17,45 @@ CREATE TABLE users (
 -- Quests table
 CREATE TABLE quests (
     id BIGSERIAL PRIMARY KEY,
-    quest_id VARCHAR(255) NOT NULL,
+    quest_id VARCHAR(255) NOT NULL UNIQUE,
     client_id VARCHAR(255) NOT NULL,
     dev_id VARCHAR(255),
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     status VARCHAR(50) NOT NULL DEFAULT 'NotStarted',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (client_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (dev_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 -- Reward table
 CREATE TABLE reward (
     id BIGSERIAL PRIMARY KEY,
-    quest_id BIGINT NOT NULL REFERENCES quests(id),
+    quest_id VARCHAR(255) NOT NULL,
     base_reward NUMERIC NOT NULL,
     time_reward NUMERIC NOT NULL,
     completion_reward NUMERIC NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (quest_id) REFERENCES quests(quest_id) ON DELETE CASCADE
+);
+
+-- Dev Submissions (uploads) table
+CREATE TABLE dev_submissions (
+    id BIGSERIAL PRIMARY KEY,
+    client_id VARCHAR(255) NOT NULL,
+    dev_id VARCHAR(255) NOT NULL,
+    quest_id VARCHAR(255) NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    file_type TEXT,
+    file_size BIGINT,
+    s3_object_key TEXT NOT NULL UNIQUE,
+    bucket_name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMPTZ,
+    FOREIGN KEY (client_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (dev_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (quest_id) REFERENCES quests(quest_id) ON DELETE CASCADE
 );
